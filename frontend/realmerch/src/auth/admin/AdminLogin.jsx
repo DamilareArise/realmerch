3c4cc25cfb8565d8b137b2e9d9255cb5c6b4a331
Onifade Joshua <onifadjosh@gmail.com>
2024-10-08 20:36:01 +0100

    Updated AdminLogin component with minor styling changes and added icons for email and password inputs.

--- frontend/realmerch/src/auth/admin/AdminLogin.jsx
+++ frontend/realmerch/src/auth/admin/AdminLogin.jsx
@@ -1,5 +1,7 @@
-import  { useState } from "react";
+import { useState } from "react";
 import loginImage from "././../../assets/adminLogin.jpg";
+import message from "././../../assets/mail.svg";
+import passwordd from "././../../assets/password.svg"; // Replace this with a lock icon image
 
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
@@ -38,11 +40,11 @@ const AdminLogin = () => {
 
   return (
     <div className="h-screen flex bg-[#F9F2F2]">
-      <div 
+      <div
         className="w-1/2 h-full bg-cover bg-center flex justify-center items-center"
         style={{
           backgroundImage: `url(${loginImage})`,
-          clipPath: 'polygon(0 0, 70% 0, 100% 100%, 0% 100%)',
+          clipPath: "polygon(0 0, 70% 0, 100% 100%, 0% 100%)",
         }}
       >
         <p className="text-[#FFFFFF] text-[60px] leading-[75px] font-[600]">
@@ -50,43 +52,60 @@ const AdminLogin = () => {
         </p>
       </div>
 
-      <div className="w-1/2 p-8 flex justify-center flex-col">
-        <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">Sign In</h2>
+      <div className="w-1/2 p-8 flex justify-center flex-col items-center">
+        <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+          Sign In
+        </h2>
 
-        <form onSubmit={handleSubmit}>
-          {/* Email Input */}
+        <form onSubmit={handleSubmit} className="w-[520px]">
           <div className="mb-4">
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={`w-full px-4 py-[17px] border border-[#808080] rounded-lg focus:outline-none /focus:ring-2 /focus:ring-blue-600 bg-[#F9F2F2] ${
-                errors.email ? "border-red-500" : ""
-              }`}
-              placeholder="Email"
-            />
-            {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
+            <div
+              className={`flex w-full py-[17px] px-4 border ${
+                errors.email ? "border-red-500" : "border-[#808080]"
+              } rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 bg-[#F9F2F2]`}
+            >
+              <img src={message} alt="Email" width={22.07} height={17.43} />
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full pl-4 bg-transparent focus:outline-none"
+                placeholder="Email"
+              />
+            </div>
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+            )}
           </div>
 
-          {/* Password Input */}
           <div className="mb-4">
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className={`w-full px-4 py-[17px] border border-[#808080] bg-[#F9F2F2] rounded-lg focus:outline-none /focus:ring-2 focus:ring-blue-600 ${
-                errors.password ? "border-red-500" : ""
-              }`}
-              placeholder="Password"
-            />
-            {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
+            <div
+              className={`flex w-full py-[17px] px-4 border ${
+                errors.password ? "border-red-500" : "border-[#808080]"
+              } rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 bg-[#F9F2F2]`}
+            >
+              <img src={passwordd} alt="Password" width={22.07} height={17.43} />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full pl-4 bg-transparent focus:outline-none"
+                placeholder="Password"
+              />
+            </div>
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+            )}
           </div>
 
           {/* Forgot Password Link */}
           <div className="flex justify-end mb-6">
-            <a href="#" className="text-[18px] font-[400] text-black hover:underline text-end">
+            <a
+              href="#"
+              className="text-[18px] font-[400] text-black hover:underline text-end"
+            >
               Forgot Password?
             </a>
           </div>
@@ -94,7 +113,7 @@ const AdminLogin = () => {
           {/* Submit Button */}
           <button
             type="submit"
-            className="w-full bg-[#845649] rounded-tl-lg rounded-br-lg text-white font-bold py-[17px] px-4 /hover:rounded-lg hover:opacity-[75%] transition duration-300"
+            className="w-full bg-[#845649] rounded-tl-[40px] rounded-br-[40px] text-white font-bold py-[17px] px-4 hover:opacity-[75%] transition duration-300"
           >
             Login
           </button>
---
+++ frontend/realmerch/src/components/AdminDash/SideNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideNav = () => {
+  return (
+    <div className='fixed w-[150px] bg-[#845649] px-[40px] py-[28px]'>
+
+    </div>
+  )
+}
+
+export default SideNav
