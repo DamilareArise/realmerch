38840f6ad38c5c0f025b3520b2ecbd9212dc26f8
Onifade Joshua <onifadjosh@gmail.com>
2024-12-06 23:16:29 +0100

    Refactor Top Selling Product component and styles

--- frontend/realmerch/src/components/AdminDash/TopSellingSection.jsx
+++ frontend/realmerch/src/components/AdminDash/TopSellingSection.jsx
@@ -5,7 +5,7 @@ const TopSellingSection = () => {
   return (
     <div>
       <p className="text-[20px] md:text-[34px] font-[500] mb-[16px] md:mb-[24px]">Top Selling Product</p>
-      <div className="flex flex-wrap /justify-between gap-[18px] xl:gap-[30px]">
+      <div className="flex flex-wrap /justify-between gap-[10px] md:gap-[18px] xl:gap-[30px]">
         <TopSellingCard />
         <TopSellingCard />
         <TopSellingCard />
